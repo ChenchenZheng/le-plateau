23ab72f2af96759d444676b7c4b06729dc3010bd
Chenchen Zheng <chenchenzheng42@gmail.com>
2021-06-02 09:54:58 +0200

    fix flatpickr issue

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -2,11 +2,10 @@ import flatpickr from "flatpickr";
 import rangePlugin from "flatpickr/dist/plugins/rangePlugin";
 
 const initFlatpickr = () => {
-  flatpickr("#range_start", {
-    minDate: "today",
+  flatpickr("#event_start_time", {
     enableTime: true,
     altInput: true,
-    plugins: [new rangePlugin({ input: "#range_end"})]
+    plugins: [new rangePlugin({ input: "#event_end_time"})]
   });
 }
 
