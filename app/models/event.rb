55fcbc3095d6e8030172b13e6e32d7f9301a1930
Chenchen Zheng <chenchenzheng42@gmail.com>
2021-06-15 10:27:46 +0200

    🎨 Better syntax

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,23 +4,17 @@ class Event < ApplicationRecord
   has_many :reviews
   has_many :participations, dependent: :destroy
   has_one :chatroom, dependent: :destroy
-  # has_many :participations
   has_many :users, through: :participations
   validates :address, presence: true
   validates :description, length: { minimum: 20 }, presence: true
   validates :nb_players, presence: true
   validates_numericality_of :nb_players, greather_than: 0
   validates :start_time, presence: true
-  # validates :end_time, presence: true
   validates :city, presence: true
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 
   def completion_percentage
-    if self.nb_players > 0
-      (((self.participations.select { |p| p.statut == "Accepted" }.count + 1).to_f / (self.nb_players).to_f) * 100).to_i
-    else
-      0
-    end
+    nb_players.positive? ? (((participations.select { |p| p.statut == "Accepted" }.count + 1) / nb_players.to_f) * 100).to_i : 0
   end
 end
